59de14409aa3b30447ae66bc1ced8ff6e7f64ec8
Boubker BENNANI <boubker.bennani@etu.imt-nord-europe.fr>
2024-06-03 12:10:01 +0200

    Update random_cursor_wander.py

--- random_cursor_wander.py
+++ random_cursor_wander.py
@@ -1,10 +1,13 @@
+import pyautogui
+import random
+import time
+
+pyautogui.FAILSAFE = False
+
 def move_mouse():
-    import pyautogui
-    import random
-    import time
     while True:
-        x = random.randint(0,1920)
-        y = random.randint(0,1080)
+        x = random.randint(0, 1920)  
+        y = random.randint(0, 1080)
         pyautogui.moveTo(x, y, duration=0.25)
         time.sleep(5)
 
